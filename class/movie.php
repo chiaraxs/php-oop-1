b91c6c9687b208bd67d462792b03a06c0ffa86f7
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-25 13:19:35 +0100

    Init

---
+++ class/movie.php
@@ -0,0 +1,6 @@
+<!-- create un file index.php in cui:
+- è definita una classe ‘Movie’
+   => all’interno della classe sono dichiarate delle variabili d’istanza
+   => all’interno della classe è definito un costruttore
+   => all’interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
